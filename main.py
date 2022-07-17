61853e8a23e4d5efa7b492e6372de83da1d2315d
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-07-17 15:20:03 +0100

    added main menu labels into grid

--- main.py
+++ main.py
@@ -2,8 +2,19 @@
 
 import tkinter as tk
 
+FONT = ('Arial', 20, 'normal')
+FONT_BOLD = ('Arial', 20, 'bold')
+
 root = tk.Tk()
 root.title('Diabetes Manager')
+
+title_label = tk.Label(root, text='Diabetes Manager', font=FONT_BOLD)
+record_btn = tk.Button(root, text='Record reading')
+trend_btn = tk.Button(root, text='Trends')
+
+title_label.grid(column=1, row=0, columnspan=2)
+record_btn.grid(column=0, row=1)
+trend_btn.grid(column=3, row=1)
 
 
 root.mainloop()
