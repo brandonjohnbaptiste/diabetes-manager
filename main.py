68b04c715c1a1b7a381e92344d3ad4dbf0919b32
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-07-17 15:25:58 +0100

    updated minimum window size

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 
 import tkinter as tk
 
+HEIGHT = 800
+WIDTH = 600
 FONT = ('Arial', 20, 'normal')
 FONT_BOLD = ('Arial', 20, 'bold')
 
 root = tk.Tk()
+root.minsize(width=WIDTH, height=HEIGHT)
 root.title('Diabetes Manager')
 
 title_label = tk.Label(root, text='Diabetes Manager', font=FONT_BOLD)
-record_btn = tk.Button(root, text='Record reading')
+record_btn = tk.Button(root, text='Record')
 trend_btn = tk.Button(root, text='Trends')
 
 title_label.grid(column=1, row=0, columnspan=2)
