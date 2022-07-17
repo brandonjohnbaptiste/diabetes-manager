f0d705c3009513663b927df56c3b8d4e7157473f
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-07-17 15:42:38 +0100

    vertical alignment for main menu

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ FONT_BOLD = ('Arial', 20, 'bold')
 root = tk.Tk()
 root.minsize(width=WIDTH, height=HEIGHT)
 root.title('Diabetes Manager')
+root.grid_columnconfigure((0, 1, 2, 3), weight=1)
 
 title_label = tk.Label(root, text='Diabetes Manager', font=FONT_BOLD)
 record_btn = tk.Button(root, text='Record')
